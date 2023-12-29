ec2a608ee2f3129d334a0fdcb91167f0b5ab2d88
BILALck4 <chekhchou.bilal@etu.uae.ac.ma>
2023-12-29 23:14:41 +0100

    wallet button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { WalletButtonComponent } from "./wallet-button/wallet-button.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, WalletButtonComponent]
 })
 export class AppComponent {
   title = 'Assurance-Front';
--- src/app/wallet-button/wallet-button.component.ts
+++ src/app/wallet-button/wallet-button.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-wallet-button',
+  imports: [MatButtonModule, MatIconModule], 
   standalone: true,
-  imports: [],
   templateUrl: './wallet-button.component.html',
   styleUrl: './wallet-button.component.css'
 })
 export class WalletButtonComponent {
+
 
 }
