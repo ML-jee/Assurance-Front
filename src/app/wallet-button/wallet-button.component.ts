1e6e9534296216bd3395fca4233601197234751e
BILALck4 <chekhchou.bilal@etu.uae.ac.ma>
2023-12-29 20:11:37 +0100

    wallet component

---
+++ src/app/wallet-button/wallet-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wallet-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './wallet-button.component.html',
+  styleUrl: './wallet-button.component.css'
+})
+export class WalletButtonComponent {
+
+}
