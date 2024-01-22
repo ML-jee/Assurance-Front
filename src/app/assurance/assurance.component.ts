2d8376c36168190646da490b5fc567aee38c250b
maroua <marouaamal2002@gmail.com>
2024-01-22 21:14:09 +0100

    test

---
+++ src/app/assurance/assurance.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assurance',
+  standalone: true,
+  imports: [],
+  templateUrl: './assurance.component.html',
+  styleUrl: './assurance.component.css'
+})
+export class AssuranceComponent {
+
+}
