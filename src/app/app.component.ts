0284495123bf0f1da76998922c4ddb1f800ba104
BILALck4 <chekhchou.bilal@etu.uae.ac.ma>
2024-01-24 13:59:45 +0100

    adding componenets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,14 @@ import { RouterOutlet } from '@angular/router';
 import { WalletButtonComponent } from "./wallet-button/wallet-button.component";
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ConnectionComponent } from "./connection/connection.component";
+import {FormComponent } from "./form/form.component";
+import { BlockchaineSectionComponent } from "./blockchaine-section/blockchaine-section.component";
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, NavBarComponent, ConnectionComponent]
+    imports: [CommonModule, RouterOutlet, NavBarComponent, ConnectionComponent, FormComponent, BlockchaineSectionComponent]
 })
 
 export class AppComponent {
